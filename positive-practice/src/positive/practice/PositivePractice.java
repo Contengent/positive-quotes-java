3d0298097d4e2049a4a204d5fd08165f783b52a5
Contengent <103345406+Contengent@users.noreply.github.com>
2022-06-03 00:28:17 -0400

    Add files via upload

---
+++ positive-practice/src/positive/practice/PositivePractice.java
@@ -0,0 +1,24 @@
+/**
+ * @author 
+ *
+ *   /\_/\  _     ___
+ *  |>'v'<||"| -/\, ,`>
+ *     -=Edelstein=-
+ * 
+ */
+package positive.practice;
+
+/**
+ *
+ * @author oxygen
+ */
+public class PositivePractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
